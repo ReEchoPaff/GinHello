6110726f3dbcec41d6330e3ef876bdd3e0196734
ReEchoPaff <guozerun33@163.com>
2019-12-20 16:11:58 +0800

    gin8 修改完毕，成功运行。

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,13 +1,20 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		fmt.Println("已经授权")
-		context.Next()
+		cookie, e := context.Request.Cookie("user_cookie")
+		if e == nil {
+			context.SetCookie(cookie.Name, cookie.Value, 1000, cookie.Path, cookie.Domain, cookie.Secure, cookie.HttpOnly)
+			context.Next()
+		} else {
+			context.Abort() // 终止当前请求
+			context.HTML(http.StatusUnauthorized, "401.tmpl", nil)
+		}
+
 	}
 }
